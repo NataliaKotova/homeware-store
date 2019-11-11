b99b19c28692f6abf5852da0d9de6824453358df
Natalia Kotova <natalia.v.kotova@gmail.com>
2019-11-12 10:29:32 +1300

    final document

---
+++ js/script.js
@@ -0,0 +1,464 @@
+//created objects array
+var homeware = [
+    {
+        name: "Pillows",
+        id: "1",
+        materials: "Fabric - modern curtain velvet. ",
+        color: "Multicolored",
+        size: "50 x 50CM",
+        description: "Actual shades, short pile, matte, universal collection, suitable for any interior.",
+        photo1: "images/pillows1.jpg",
+        photo2: "images/pillows1.jpg",
+        price:120,
+        category: "Textile",
+        author: ""
+    },
+    {
+        name: "Bottles",
+        id: "2",
+        materials: "Glass",
+        color: "Multicolored",
+        size: "Height: 25CM, Diametr: 17CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/bottles.jpg",
+        photo2: "images/bottles.jpg",
+        price:40,
+        category: "Home decor",
+        author: "Agata Create, Upslash"
+    },
+    {
+        name: "Mirror",
+        id: "3",
+        materials: "OUTER SHELL, 40% MDF, 40% GLASS, 20% ALUMINIUM",
+        color: "Black",
+        size: "Diametr: 70CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/mirror.jpg",
+        photo2: "images/mirror.jpg",
+        price:40,
+        category: "Homedecor",
+        author:""
+    },
+    {
+        name: "Armchair",
+        id: "4",
+        materials: "Upholstery: 50% Polyester, 50% Acrylic. Foam filled seat cushion and back rest. Wooden leg construction.",
+        color: "Blue",
+        size: "71 x 77.5 x 84cm (wxdxh)",
+        description: "Sit back and relax in this comfortable armchir.",
+        photo1: "images/armchair.jpg",
+        photo2: "images/armchair.jpg",
+        price:440,
+        category:"Furniture",
+        author: "Kari Shea, Upslash"
+    },
+    {
+        name: "Plant",
+        id: "5",
+        materials: "Natural plant in ceramic pot",
+        color: "Green",
+        size: "Height: 30CM, Width: 18CM",
+        description: "Beautiful plant, great addition to any interior.",
+        photo1: "images/plant.jpg",
+        photo2: "images/plant.jpg",
+        price:30,
+        category:"",
+        author: "Kari Shea, Upslash"
+    },
+
+    {
+        name: "Green sofa",
+        id: "6",
+        materials: "Velvet",
+        color: "Green",
+        size: "",
+        description: "Excellent addition to any interior.",
+        photo1: "images/sofa-green1.jpg",
+        photo2: "images/sofa-green2.jpg",
+        price:840,
+        category: "Furniture",
+        author:"Phillip Goldsberry, Upslash"
+    },
+    {
+        name: "Leather sofa",
+        id: "7",
+        materials: "Leather",
+        color: "Brown",
+        size: "",
+        description: "Excellent addition to any interior.",
+        photo1: "images/sofa-brown1.jpg",
+        photo2: "images/sofa-brown1.jpg",
+        price:2000,
+        category: "Furniture",
+        author:"Paul Weaver, Upslash </br> Kara Eads, Upslash"
+    },
+    {
+        name: "Blanket",
+        id: "8",
+        materials: "100% cotton",
+        color: "White",
+        size: "200x200CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/blanket1.jpg",
+        photo2: "images/blanket2.jpg",
+        price:40,
+        category: "Textile",
+        author:"Agata Create, Upslash"
+    },
+    {
+        name: "Chandelier",
+        id: "9",
+        materials: "Bone",
+        color: "White",
+        size: "Diametr: 70CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/chandelier1.jpg",
+        photo2: "images/chandelier2.jpg",
+        price:40,
+        category: "Lighting",
+        author:"Lindie Wilton, Upslash"
+    },
+    {
+        name: "Curtains",
+        id: "10",
+        materials: "OUTER SHELL, 40% MDF, 40% GLASS, 20% ALUMINIUM",
+        color: "White",
+        size: "Diametr: 70CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/curtains1.jpg",
+        photo2: "images/curtains1.jpg",
+        price:40,
+        category: "Textile",
+        author:"Christopher Martyn, Upslash"
+    },
+    {
+        name: "Picture",
+        id: "11",
+        materials: "OUTER SHELL, 40% MDF, 40% GLASS, 20% ALUMINIUM",
+        color: "Black",
+        size: "Diametr: 70CM",
+        description: "Excellent addition to any interior.",
+        photo1: "images/picture1.jpg",
+        photo2: "images/picture1.jpg",
+        price:40,
+        category: "Wallart",
+        author:"Kari Shea, Upslash"
+    },
+    {
+        name: "Chair",
+        id: "12",
+        materials: "OUTER SHELL, 40% MDF, 40% GLASS, 20% ALUMINIUM",
+        color: "White",
+        size: "",
+        description: "Excellent addition to any interior.",
+        photo1: "images/chair1.jpg",
+        photo2: "images/chair2.jpg",
+        price:40,
+        category:"Furniture",
+        author:"Agata Create, Sarah Dorweiler, Upslash"
+    },
+    {
+        name: "Armchair",
+        id: "13",
+        materials: "OUTER SHELL, 40% MDF, 40% GLASS, 20% ALUMINIUM",
+        color: "Yellow",
+        size: "",
+        description: "Excellent addition to any interior.",
+        photo1: "images/armchair-yellow1.jpg",
+        photo2: "images/armchair-yellow2.jpg",
+        price:40,
+        category:"Furniture",
+        author:"Kari Shea, Upslash"
+    },
+    {
+        name: "Print",
+        id: "14",
+        materials: "",
+        color: "Yellow",
+        size: "",
+        description: "Acrilic paint. Excellent addition to any interior.",
+        photo1: "images/print1.jpg",
+        photo2: "images/print2.jpg",
+        price:100,
+        category:"Wallart",
+        author:"Olia Gozha, Upslash"
+    },
+    {
+        name: "Print",
+        id: "15",
+        materials: "",
+        color: "Pink",
+        size: "",
+        description: "Acrilic paint. Excellent addition to any interior.",
+        photo1: "images/pink-print1.jpg",
+        photo2: "images/pink-print1.jpg",
+        price:40,
+        category:"wallart",
+        author:"Stephanie Harvey, Upslash"
+    },
+    {
+        name: "Basket",
+        id: "16",
+        materials: "",
+        color: "Natural",
+        size: "",
+        description: "Excellent addition to any interior.",
+        photo1: "images/basket1.jpg",
+        photo2: "images/basket1.jpg",
+        price:40,
+        category: "Homedecor",
+        author:"Agata Create, Upslash"
+    },
+]
+
+//created function to show items from the sertain array which i pass into function as a parameter
+function showItems(array) {
+    document.getElementById('homeware').innerHTML = " ";
+    for(var i = 0; i < array.length; i++) {
+        document.getElementById('homeware').innerHTML+='<div class="col-md-3">'
+        +'<div class="frame">'
+        +'<div class="card m-2">'
+        +'<img class="card-img-top" src="'+ array[i].photo1 +' " alt="Homeware">'
+        +'<div class="card-body m-0">'
+        +'<p class="card-text text-center mb-0">' + array[i].name.toUpperCase() + '</p>'
+        +'<p class="card-text text-center mb-0">' + "$" + array[i].price + '</p>'
+        +'</div>'
+        +'<button id="'+(i+1)+'" class="quickView" data-product="'+array[i].id+'" type="button" data-toggle="modal" data-target="#exampleModal">QUICK VIEW</button>'
+        +'</div>'
+        +'</div>'
+        +'</div>'
+    }
+}
+//call function first to show all items from homeware array on landing page
+showItems(homeware);
+
+//added event listener on the "reset" button
+document.getElementById('reset').addEventListener('click', function() {
+    showItems(homeware);
+});
+
+//created function constructor to create object where I'll store key values 
+//as a boolean dtat type for Categories and Colors checkboxes
+
+function FilterCriteria() {
+    this.categories = {};//object for ticked categories
+    this.colors = {};//object for ticked colors
+    this.shouldFilterByCategory = function() {
+        for (var key in this.categories) {
+            if (this.categories.hasOwnProperty(key)) {
+                if(this.categories[key]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    this.shouldFilterByColor = function() {
+        for (var key in this.colors) {
+            if(this.colors.hasOwnProperty(key)) {
+                if(this.colors[key]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+//create function which will store all objects and we can manipulate with key values in it
+var selectedCheckboxes = new FilterCriteria();
+
+// the below is the example of object, how this data type looks like
+// var selectedCheckboxes = {
+//     categories: {},
+//         categories: {
+//              "furniture":true,
+//              "homeDecor":false
+//          }
+//     colors: {"green": true, "brown": false};
+//     object: {key1: value1, key2: value2};
+// }
+
+
+
+//created new array to push all ticked colors and categories
+var selectedItems = [];
+
+//created function to push objects from array depending on condtions
+//these both functions will be excecuted in the next four functions
+function filterCategory(products) {
+    var filteredProducts=[];
+    for (var i = 0; i < products.length; i++) {
+        if (selectedCheckboxes.categories[products[i].category.toLowerCase()] ) {
+            filteredProducts.push(products[i]);
+        }  
+        //check when none of checkboxes have been chosen
+        if (selectedCheckboxes.shouldFilterByCategory() === false) {
+            filteredProducts.push(products[i]);
+        }
+    }
+    return filteredProducts;
+}
+
+function filterColor(products) {
+    var filteredProducts=[];
+    for (var i = 0; i < products.length; i++) {
+        if (selectedCheckboxes.colors[products[i].color.toLowerCase()] ) {
+            filteredProducts.push(products[i]);
+        } 
+        if (selectedCheckboxes.shouldFilterByColor() === false) {
+            filteredProducts.push(products[i]);
+        }
+    }
+    return filteredProducts;
+}
+
+//this function is responding on category checkbox click and it displays result as a list of products
+//function where all above functions that I've created are executed to get the final objects array
+
+$(".category").on("click", function() {
+    //to get the id of the clicked checkbox
+    var selectedCategoryID = jQuery(this).attr('id'); 
+    console.log(selectedCategoryID);
+    //after i need to check if the checkbox ticked or unticked
+    if (jQuery(this).is(":checked")) { 
+        selectedCheckboxes.categories[selectedCategoryID] = true;//store key values in object, if checkbox is ticked or not ticked
+    }
+    //store key value (true or false) in object
+    else {
+        selectedCheckboxes.categories[selectedCategoryID] = false;
+    }
+    var filteredProducts = filterCategory(homeware);//store new objects array after filterCategory(array) function
+    filteredProducts = filterColor(filteredProducts);//in the same variable store final objects array 
+    showItems(filteredProducts);
+});
+
+$(".color").on("click", function() {
+    var selectedColorID = jQuery(this).attr('id'); //to get the id of checked checkbox
+    console.log(selectedColorID);
+    if (jQuery(this).is(":checked")) {
+        selectedCheckboxes.colors[selectedColorID] = true;// now we need to stor information in object that attribute checked of this checkbox is checked 
+    }
+    else {
+        selectedCheckboxes.colors[selectedColorID] = false;
+    }
+    var filteredProducts = filterCategory(homeware);
+    filteredProducts = filterColor(filteredProducts);
+    showItems(filteredProducts);
+});
+
+
+function sortLow() {
+    var filteredProducts = filterCategory(homeware);
+    filteredProducts = filterColor(filteredProducts);
+    var sortedItems = filteredProducts.sort((function(a, b){
+        if(a.price < b.price) {
+            return -1;
+        }
+        if(a.price > b.price) {
+            return 1;
+        }
+        return 0;
+        }));
+        showItems(sortedItems);
+  }
+
+function sortHigh() {
+    var filteredProducts = filterCategory(homeware);
+    filteredProducts = filterColor(filteredProducts);
+    var sortedHomeware = filteredProducts.sort((function(a, b){
+    if(a.price < b.price) {
+    return 1;
+    }
+    if(a.price > b.price) {
+    return -1;
+    }
+    return 0;
+    }));
+    showItems(sortedHomeware);
+}
+
+document.getElementById('pricelow').addEventListener('click', function() {
+    sortLow();
+});
+
+document.getElementById('pricehigh').addEventListener('click', function() {
+    sortHigh();
+});
+
+$('.quickView').on('click', function(){
+    var id = $(this).attr('data-product');
+    for (var i =0; i <homeware.length; i++) {
+        if(homeware[i].id === id) {
+            var homewareProduct = homeware[i];
+        }
+    }
+        document.getElementById('modalContent').innerHTML
+        ='<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'
+        +'<div class="modal-dialog" role="document">'
+        +'<div class="modal-content">'
+        +'<div class="modal-header">'
+        +'<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+        +'<span aria-hidden="true">&times;</span>'
+        +'</button>'
+        +'</div>'
+        +'<div class="modal-body">'
+        +'<div class="row flex-container">'
+        +'<div id="carouselExampleIndicators" class="carousel slide col-md-6">'
+        +'<ol class="carousel-indicators">'
+        +'<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>'
+        +'<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>'
+        +'</ol>'
+            +'<div class="carousel-inner">'
+                +'<div class="carousel-item active">'
+                    +'<div  img-center-vertical>'
+                    +'<img class="d-block w-100" src="'+homewareProduct.photo1+'" alt="First slide">'
+                    +'</div>'
+                +'</div>'
+                +'<div class="carousel-item ">'
+                    +'<div  img-center-vertical>'
+                    +'<img class="d-block w-100" src="'+homewareProduct.photo2+'" alt="Second slide">'
+                    +'</div>'
+                +'</div>'
+            +'<div>'
+        +'</div>'
+        +'</div>'
+        +'<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">'
+        +'<span class="carousel-control-prev-icon" aria-hidden="true"></span>'
+        +'<span class="sr-only">Previous</span>'
+        +'</a>'
+        +'<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">'
+        +'<span class="carousel-control-next-icon" aria-hidden="true"></span>'
+        +'<span class="sr-only">Next</span>'
+        +'</a>'
+        +'</div>'
+        +'<div class="col-md-6">'
+        +'<h2 class="mb-0 productName text-center">'+ homewareProduct.name.toUpperCase() + '</h2></br>'
+        +'<h4 class="mb-0">Price: $'+ homewareProduct.price + '</h4></br>'
+        +'<hr class="mt-0 line">'
+        +'<span>PRODUCT DESCRIPTION: </br>'+ homewareProduct.description + '</span></br>'
+        +'<div class="mt-3 mb-3">'
+        +'<button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'
+        +'Choose amount</button>'
+        +'<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">'
+        +'<a class="dropdown-item">1</a>'
+        +'<a class="dropdown-item">2</a>'
+        +'<a class="dropdown-item">3</a>'
+        +'<a class="dropdown-item">4</a>'
+        +'</div>'
+        +'<button class="btn pinkBtn ml-1 " type="button">ADD TO CART</button>'
+        +'</div>'
+        +'</div>'
+        +'</div>'//carousel slide ENDS
+        +'</div>'//row ENDS
+        +'</div>'//modal body ENDS
+        +'</div>'
+        +'</div>'
+        +'</div>'
+  
+  })
+
+  
+
+
+
